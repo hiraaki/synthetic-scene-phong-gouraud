19ced27b8913b2bc5e17057d684a211b29c142e5
mauricio.ishida <mauricio.ishida@ecocentauro.com.br>
2022-07-20 10:26:50 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from models.Vertex import Vertex
+from models.Sphere import Sphere
+from models.GeometricTransformation import GeometricTransformation as gt
+
+
+window = Tk()
+window.title('Computação gráfica: modelagem de cenas')
+
+canvas = Canvas(window, width = 800, height = 600)
+canvas.pack()
+
+sphere = Sphere(100, 8, 8)
+
+# for vertex in sphere.vertexes:
+  # print(vertex.coordinatesXYZ())
+  # gt.translation(vertex, Vertex(400, 300, 1))
+
+VRP = Vertex(200, 200, 200)
+focalPoint = Vertex(150, 150, 150)
+viewUp = Vertex(0, 1, 0)
+dp = 175
+
+gt.calculateSRCVertexes(sphere.vertexes, VRP, focalPoint, viewUp)
+gt.calculateSRTVertexes(sphere.vertexes, Vertex(-400, -300, 1), Vertex(400, 300, 1), Vertex(0, 0, 1), Vertex(800, 600, 1), focalPoint.z,dp)
+
+for edge in sphere.edges:
+  canvas.create_line(edge.startVertex.coordinatesXY(), edge.endVertex.coordinatesXY())
+  # print(edge.startVertex.coordinates(), edge.endVertex.coordinates())
+
+for v in sphere.vertexes:
+  print(v.coordinatesXYZ())
+
+window.mainloop()
+
