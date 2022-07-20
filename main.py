e4198cfc447c08d94fe5839ee71829172fef15ea
Maurcio Ishida <mauricio.ishida@ecocentauro.com.br>
2022-07-20 12:20:05 -0300

    compostMatriz

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from tkinter import *
 from models.Vertex import Vertex
 from models.Sphere import Sphere
 from models.GeometricTransformation import GeometricTransformation as gt
+from models.Scene import Scene
 
 
 window = Tk()
 window.title('Computação gráfica: modelagem de cenas')
 
-canvas = Canvas(window, width = 800, height = 600)
-canvas.pack()
 
 sphere = Sphere(100, 8, 8)
 
@@ -21,15 +20,27 @@ focalPoint = Vertex(150, 150, 150)
 viewUp = Vertex(0, 1, 0)
 dp = 175
 
-gt.calculateSRCVertexes(sphere.vertexes, VRP, focalPoint, viewUp)
-gt.calculateSRTVertexes(sphere.vertexes, Vertex(-400, -300, 1), Vertex(400, 300, 1), Vertex(0, 0, 1), Vertex(800, 600, 1), focalPoint.z,dp)
+minWindow=Vertex(-400, -300, 1)
+maxWindow=Vertex(400, 300, 1)
+minView=Vertex(0, 0, 0)
+maxView=Vertex(600, 400, 0)
 
-for edge in sphere.edges:
-  canvas.create_line(edge.startVertex.coordinatesXY(), edge.endVertex.coordinatesXY())
-  # print(edge.startVertex.coordinates(), edge.endVertex.coordinates())
+canvas = Canvas(window, width=maxView.x, height= maxView.y)
+canvas.pack()
+# vertexMinWindow, vertexMaxWindow, vertexMinView, vertexMaxView, vrp, focalPoint, viewUp,dp, canvas
+scene = Scene(minWindow,maxWindow,minView,maxView,VRP,focalPoint,viewUp,dp,canvas)
 
-for v in sphere.vertexes:
-  print(v.coordinatesXYZ())
+scene.Draw()
+
+# gt.calculateSRCVertexes(sphere.vertexes, VRP, focalPoint, viewUp)
+# gt.calculateSRTVertexes(sphere.vertexes,Vertex(-400, -300, 1), Vertex(400, 300, 1), Vertex(0, 0, 1), Vertex(800, 600, 1), focalPoint.z,dp)
+
+# for edge in sphere.edges:
+#   canvas.create_line(edge.startVertex.coordinatesXY(), edge.endVertex.coordinatesXY())
+#   # print(edge.startVertex.coordinates(), edge.endVertex.coordinates())
+
+# for v in sphere.vertexes:
+#   print(v.coordinatesXYZ())
 
 window.mainloop()
 
--- models/Scene.py
+++ models/Scene.py
@@ -1,15 +1,25 @@
+from numpy import append
 from models.GeometricTransformation import GeometricTransformation as gt
+from models.Vertex import Vertex
 
 class Scene:
-    def __init__(self, vertexMinWindow, vertexMaxWindow, vertexMinView, vertexMaxView, dp):
+    def __init__(self,vertexMinWindow, vertexMaxWindow, vertexMinView, vertexMaxView, vrp, focalPoint, viewUp,dp, canvas):
+        self.canvas = canvas
         self.vertexMinWindow = vertexMinWindow
         self.vertexMaxWindow = vertexMaxWindow 
         self.vertexMinView = vertexMinView
         self.vertexMaxView=vertexMaxView
         self.dp=dp
-        self.objects
-        
-    def convertion(self, vertexes, n, v, u, vrpT):   
+        self.faces = []
+        self.edges = []
+        self.vertexes = []
+        self.viewUp = viewUp
+        self.vrp = vrp
+        self.focalPoint = focalPoint
+    
+    # stru2srt composted matriz multiplication linearization
+    # {{j1,0,0,j2},{0,j3,0,j4},{0,0,1,0},{0,0,0,1}}*{{1,0,0,0},{0,1,0,0},{0,0,p1,p2},{0,0,p3,p4}}*{{r1,r2,r3,r4},{r5,r6,r7,r8},{r9,r10,r11,r12},{0,0,0,1}}*{{x},{y},{z},{1}}
+    def sruToSrt(self, vertexes, n, v, u, vrpT):   
         r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12=n.coordinatesXYZ(),vrpT.x,v.coordinatesXYZ(),vrpT.y,u.coordinatesXYZ(),vrpT.z
         p1,p2,p3,p4=1,1,-(1/self.dp),0
         j1=(self.vertexMaxView.x-self.vertexMinView.x)/(self.vertexMaxWindow.x-self.vertexMinWindow.x)
@@ -17,11 +27,25 @@ class Scene:
         j3=(self.vertexMinView.y-self.vertexMaxView.y)/(self.vertexMaxWindow.y-self.vertexMinWindow.y)
         j4=(self.vertexMinWindow.y*j3)+self.vertexMinView.y
         
+        todraw = []
+
         for v in vertexes:
             u=p3*r10*v.y+p3*r11*v.z+p3*r12+p3*r9*v.x+p4
-            v.x=v.x*(j1*r1+j2*p3*r9)+v.y*(j1*r2+j2*p3*r10)+v.z*(j1*r3+j2*p3*r11)+j1*r4+j2*p3*r12+j2*p4
-            v.y=v.y*(j3*r6+j4*p3*r10)+v.z*(j3*r7+j4*p3*r11)+v.x*(j3*r5+j4*p3*r9)+j3*r8+j4*p3*r12+j4*p4
-            v.z=p1*r10*v.y+p1*r11*v.z+p1*r12+p1*r9*v.x+p2
-            
-            
+            x=(v.x*(j1*r1+j2*p3*r9)+v.y*(j1*r2+j2*p3*r10)+v.z*(j1*r3+j2*p3*r11)+j1*r4+j2*p3*r12+j2*p4)/u
+            y=(v.y*(j3*r6+j4*p3*r10)+v.z*(j3*r7+j4*p3*r11)+v.x*(j3*r5+j4*p3*r9)+j3*r8+j4*p3*r12+j4*p4)/u
+            z=(p1*r10*v.y+p1*r11*v.z+p1*r12+p1*r9*v.x+p2)/u
+            todraw.append(Vertex(x,y,z))
 
+        return todraw
+    
+    def AddObject(self, object):
+        for o in object:
+            self.vertexes.append(o.vertexes)
+            self.edges.append(o.edges)
+
+    def Draw(self):
+        todraw = self.sruToSrt(self.vertexes)     
+        for e in todraw:
+            self.canvas.create_line(self.startVertex.coordinatesXY(), self.endVertex.coordinatesXY())                
+            for v in self.vertexes:
+                print(v.coordinatesXYZ())
